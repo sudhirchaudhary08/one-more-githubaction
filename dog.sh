02cc96cd821e8bf6ddf566f79aa5b4d5fcbf65b3
sudhirchaudhary08 <158567043+sudhirchaudhary08@users.noreply.github.com>
2025-02-26 22:18:50 +0530

    Last change done

--- dog.sh
+++ dog.sh
@@ -1,4 +1,4 @@
 sudo apt install cowsay -y
-cowsay -f dog "run for me ... I'm dog and I'm driking milk "
+cowsay -f dog "run for me ... I'm dog and I'm driking milk " >> dog.txt
 grep -i "dog" dog.txt
 cat dog.txt
